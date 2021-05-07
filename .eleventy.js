e328480cf76a0eff2781de652eee9883fca857bd
Julien Rabaud <julien.rabaud@univ-pau.fr>
2021-05-07 16:10:48 +0200

    js

--- .eleventy.js
+++ .eleventy.js
@@ -71,7 +71,8 @@ module.exports = function(eleventyConfig) {
   let markdownLibrary = markdownIt({
     html: true,
     breaks: true,
-    linkify: true
+    linkify: true,
+    typographer: true
   }).use(markdownItAnchor, {
     permalink: true,
     permalinkClass: "direct-link",
